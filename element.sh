b31ae7de3578fcbd08e84e55fb841dad9db9fe29
Rvjonh <jonhvelasco3@gmail.com>
2023-03-17 00:58:16 -0400

    feat: detection of param passed

---
+++ element.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  echo your item $1
+fi
